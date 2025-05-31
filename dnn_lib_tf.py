2f717d94477ba34af01017ec0ab984f2d68c3951
Mleczek, Pawel <pawel.mleczek.praca@gmail.com>
2025-05-31 12:18:38 +0200

    add initialize_parameters function

--- dnn_lib_tf.py
+++ dnn_lib_tf.py
@@ -1,6 +1,10 @@
 import h5py
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
+from keras.src.backend import shape
+
+USE_RANDOM_SEED = True
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_signs.h5', "r")
@@ -68,3 +72,37 @@ def unroll_and_normalize(image_dataset):
     result = tf.cast(result, tf.float32) / 255.0
 
     return result
+
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with TensorFlow. The shapes are:
+                        W1 : [25, 12288]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [6, 12]
+                        b3 : [6, 1]
+
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+
+    initializer = tf.keras.initializers.GlorotNormal()
+
+    W1 = tf.Variable(initializer(shape = (25, 12288)))
+    b1 = tf.Variable(initializer(shape = (25, 1)))
+    W2 = tf.Variable(initializer(shape = (12, 25)))
+    b2 = tf.Variable(initializer(shape = (12, 1)))
+    W3 = tf.Variable(initializer(shape = (6, 12)))
+    b3 = tf.Variable(initializer(shape = (6, 1)))
+
+    parameters = {
+        "W1" : W1,
+        "b1" : b1,
+        "W2" : W2,
+        "b2" : b2,
+        "W3" : W3,
+        "b3" : b3
+    }
+
+    return parameters
