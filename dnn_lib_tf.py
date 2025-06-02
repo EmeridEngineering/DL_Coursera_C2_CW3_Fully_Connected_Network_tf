753818acad72addbddc9ab64ee51d3cc131b80a4
Mleczek, Pawel <pawel.mleczek.praca@gmail.com>
2025-06-02 16:31:36 +0200

    add categorical crossentropy loss calculation

--- dnn_lib_tf.py
+++ dnn_lib_tf.py
@@ -144,3 +144,19 @@ def one_hot_encoding(label, C=6):
 
     return tf.reshape(tf.one_hot(label, C), shape=[C,]) # reshape for confidence as one_hot shape depends on the inputs
 
+
+def compute_total_loss(logits, labels):
+    """
+    Computes the total loss
+
+    Arguments:
+    logits -- output of forward propagation (output of the last LINEAR unit), of shape (6, num_examples)
+    labels -- "true" labels vector, same shape as Z3
+
+    Returns:
+    total_loss - Tensor of the total loss value
+    """
+
+    total_loss = tf.reduce_sum(tf.keras.losses.categorical_crossentropy(tf.transpose(labels), tf.transpose(logits), from_logits = True))
+
+    return total_loss
