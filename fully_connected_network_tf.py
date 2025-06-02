6de1fb55b1789c58354a26d2d50916e2a8b18fde
Mleczek, Pawel <pawel.mleczek.praca@gmail.com>
2025-06-02 16:48:37 +0200

    add labels preprocessing

--- fully_connected_network_tf.py
+++ fully_connected_network_tf.py
@@ -15,9 +15,15 @@ display_data(x_train_initial, y_train_initial)
 unique_labels = get_unique_labels(y_train_initial)
 # print(unique_labels)
 
-# Preprocess the datasets
+# Preprocess the input datasets
 x_train = x_train_initial.map(unroll_and_normalize)
 x_test = x_test_initial.map(unroll_and_normalize)
 
 # print(x_train.element_spec)
 # print(next(iter(x_train)))
+
+# Preprocess the labels datasets
+y_train = y_train_initial.map(one_hot_encoding)
+y_test = y_test_initial.map(one_hot_encoding)
+
+# print(next(iter(y_test)))
